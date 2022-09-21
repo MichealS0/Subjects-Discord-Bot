b29609b3547314ead87fb8c4079dc935072953bd
Theron Aiolos <Awaabbkheiri7177@gmail.com>
2022-09-21 15:25:39 +0300

    Finishing Touches Are Left

--- Discord Bot/bot.py
+++ Discord Bot/bot.py
@@ -2,7 +2,7 @@ import discord
 import discord.client
 from datetime import date, time, datetime
 
-TOKEN = 'MTAyMDIxMjQ3MTg5NTY5OTQ3Ng.G7JLuA.YhmCgNjPVA6f_ERNHAvObt7yEtA7JQ6pIvpAgU'
+TOKEN = ''
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -11,12 +11,73 @@ client = discord.Client(intents=intents)
 
 def getlesson(day4subject, time4subject):
 
+    global lessonTime
+
+    # Lesson Time
+    lesson2 = "08:57"
+    lesson3 = "10:10"
+    lesson4 = "11:02"
+    lesson5 = "12:10"
+    lesson6 = "13:02"
+    lesson7 = "13:50"
+
+    # Lesson Time Thursday
+    Thu2 = "8:54"
+    Thu3 = "9:44"
+    Thu4 = "10:53"
+    Thu5 = "11:42"
+    Thu6 = "12:30"
+
     global content
     content = []
+
+    global subj
+    subj = ""
 
     with open(day4subject + ".txt", "r") as theFile:
         for subjects in theFile:
             content.append(subjects)
+
+    # Normal Days
+    if lesson2 > str(time4subject):
+        subj = content[0]
+    
+    elif lesson3 > str(time4subject):
+        subj = content[1]
+    
+    elif lesson4 > str(time4subject):
+        subj = content[2]
+    
+    elif lesson5 > str(time4subject):
+        subj = content[3]
+    
+    elif lesson6 > str(time4subject):
+        subj = content[4]
+    
+    elif lesson7 > str(time4subject):
+        subj = content[5]
+    
+    elif lesson7 < str(time4subject):
+        subj = content[6]
+    
+    # For Thursday
+    if str(time4subject) < Thu2:
+        subj = content[0]
+
+    elif str(time4subject) < Thu3:
+        subj = content[1]
+    
+    elif str(time4subject) < Thu4:
+        subj = content[2]
+    
+    elif str(time4subject) < Thu5:
+        subj = content[3]
+    
+    elif str(time4subject) < Thu6:
+        subj = content[4]
+    
+    elif str(time4subject) > Thu6:
+        subj = content[5]
 
 @client.event
 async def on_ready():
@@ -34,11 +95,11 @@ async def on_message(message):
 
             datenow = datetime.now()
             day = datenow.strftime('%A')
-            timenow = datenow.strftime("%H:%w")
+            timenow = datenow.now()
 
             getlesson(day, timenow)
 
-            await message.channel.send(content)
+            await message.channel.send("You have: " + subj)
             return
 
 client.run(TOKEN)
